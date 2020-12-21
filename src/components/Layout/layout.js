165f26b5000e299adefa4c2859bf392c79312d88
yellowfully <richenyou@gmail.com>
2020-12-21 17:05:47 +0800

    file name revised

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -8,7 +8,7 @@
 import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import Header from "../Header/header"
+import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
 import "./mystyles.css"
 import 'tachyons'
