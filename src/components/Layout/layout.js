23cf472c647f7073300b29bb89f07728d99b073a
yellowfully <richenyou@gmail.com>
2020-12-23 14:22:20 +0800

    add scrolltop z-index

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -73,7 +73,7 @@ const Layout = ({ children }) => {
       </FacebookProvider>
       {
         scrollPosition >= 1024 ?
-          <div className="tc light-blue o-80 f2 pv1 br2 fixed bottom-2 right-2 w3 h3 bw0 button-focus grow pointer" role="button" aria-label="scrolltop button" tabIndex="-2" onClick={onScrollTop} onKeyDown={onKeyScroll}>
+          <div className="tc light-blue o-80 f2 pv1 br2 fixed bottom-2 right-1 w3 h3 bw0 button-focus grow pointer z-5" role="button" aria-label="scrolltop button" tabIndex="-2" onClick={onScrollTop} onKeyDown={onKeyScroll}>
             <FontAwesomeIcon icon={faChevronUp} />
           </div>
           :
