40ecf0a6108fb1e849aac8c37a55aaae1efedab7
yellowfully <richenyou@gmail.com>
2021-03-02 17:54:30 +0800

    add matchFields to algolia query

--- gatsby-config.js
+++ gatsby-config.js
@@ -225,7 +225,7 @@ module.exports = {
         appId: process.env.GATSBY_ALGOLIA_APP_ID,
         apiKey: process.env.ALGOLIA_ADMIN_KEY,
         queries: require("./src/utils/algolia-queries"),
-        concurrentQueries: false,
+        matchFields: ['slug']
       },
     },
     {
