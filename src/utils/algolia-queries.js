0790a58101122d940fcc5ee7b0329c430ca96ac2
yellowfully <richenyou@gmail.com>
2021-03-02 17:33:50 +0800

    about-page query of algolia added

--- src/utils/algolia-queries.js
+++ src/utils/algolia-queries.js
@@ -18,37 +18,45 @@ const pageQuery = `{
   }
 }`
 
-function pageToAlgoliaRecord({ node: { id,slug,title,articles} }) {
-    return {
-        objectID:id,
-        slug:`/blog/${slug.toLowerCase()}/`,
-        title:title,
-        excerpt: articles.childMarkdownRemark.excerpt,
+function pageToAlgoliaRecord({ node: { id, slug, title, articles } }) {
+  return {
+    objectID: id,
+    slug: `/blog/${slug.toLowerCase()}/`,
+    title: title,
+    excerpt: articles.childMarkdownRemark.excerpt,
   }
 }
 
-// const aboutQuery = `{
-//     aboutPages: allContentfulAbout{
-//         edges {
-//             node {
-//                 id
-//                 slug
-//                 title
-//                 content{
-//                     childMarkdownRemark {
-//                         excerpt(format: PLAIN, pruneLength: 2000, truncate: true)
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }`
+const aboutQuery = `{
+    aboutPages: allContentfulAbout{
+        edges {
+            node {
+                id
+                slug
+                title
+                content{
+                    childMarkdownRemark {
+                        excerpt(format: PLAIN, pruneLength: 2000, truncate: true)
+                    }
+                }
+            }
+        }
+    }
+}`
 
 // function aboutToAlgoliaRecord (acc,{node: {content}}) {
 //   return (acc+content.childMarkdownRemark.excerpt)
 // }
 
-
+function aboutToAlgoliaRecord({ node: { id, slug, title, content} }) {
+  return {
+    objectID: id,
+    slug: `/about/#${slug}`,
+    title:title,
+    excerpt: content.childMarkdownRemark.excerpt,
+    ...rest
+  }
+}
 
 const projectQuery = `{
     projectPages: allContentfulProject{
@@ -72,12 +80,12 @@ const projectQuery = `{
     }
 }`
 
-function projectToAlgoliaRecord({ node: { id,slug,projectName,introduction,section } }) {
-    return {
-        objectID:id,
-        slug:`/project/${slug.toLowerCase()}/`,
-        title:projectName,
-        excerpt: section.childMarkdownRemark.excerpt+introduction.childMarkdownRemark.excerpt,
+function projectToAlgoliaRecord({ node: { id, slug, projectName, introduction, section } }) {
+  return {
+    objectID: id,
+    slug: `/project/${slug.toLowerCase()}/`,
+    title: projectName,
+    excerpt: section.childMarkdownRemark.excerpt + introduction.childMarkdownRemark.excerpt,
   }
 }
 
@@ -89,36 +97,27 @@ const queries = [
     query: pageQuery,
     transformer: ({ data }) => data.pages.edges.map(pageToAlgoliaRecord),
     indexName,
+    settings: {
+      attributesToSnippet: [`excerpt:${numberOfExcerpt}`],
+      searchableAttributes: [`title`, `excerpt`]
+    }
+  },
+  {
+    query: aboutQuery,
+    transformer: ({ data }) => data.aboutPages.edges.map(projectToAlgoliaRecord),
+    indexName,
     settings: { 
         attributesToSnippet: [`excerpt:${numberOfExcerpt}`],
         searchableAttributes:[`title`,`excerpt`]
     }
   },
-  // {
-  //   query: aboutQuery,
-  //   transformer: ({ data }) =>{
-  //     return (
-  //       {
-  //         objectID:data.aboutPages.edges[0].node.id,
-  //         slug:`/about/`,
-  //         title:`關於`,
-  //         excerpt:data.aboutPages.edges.reduce(aboutToAlgoliaRecord,'')
-  //       }
-  //     )
-  //   },
-  //   indexName,
-  //   settings: { 
-  //       attributesToSnippet: [`excerpt:${numberOfExcerpt}`],
-  //       searchableAttributes:[`title`,`excerpt`]
-  //   }
-  // },
   {
     query: projectQuery,
     transformer: ({ data }) => data.projectPages.edges.map(projectToAlgoliaRecord),
     indexName,
-    settings: { 
-        attributesToSnippet: [`excerpt:${numberOfExcerpt}`],
-        searchableAttributes:[`title`,`excerpt`]
+    settings: {
+      attributesToSnippet: [`excerpt:${numberOfExcerpt}`],
+      searchableAttributes: [`title`, `excerpt`]
     }
   }
 ]
