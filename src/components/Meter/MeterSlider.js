ecb6fca2fde4e94fbfdc851b4ebd8dfdd0e79b79
yellowfully <richenyou@gmail.com>
2021-03-08 16:44:51 +0800

    add labels of style number icons

--- src/components/Meter/MeterSlider.js
+++ src/components/Meter/MeterSlider.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { navigate } from 'gatsby'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSnowflake, faFire } from '@fortawesome/free-solid-svg-icons'
-import {iceColor,fireColor} from '../../utils/ice-fire-color'
+import { faSnowflake, faFire, faLongArrowAltLeft, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
+import { iceColor, fireColor } from '../../utils/ice-fire-color'
 
 
 const MeterSlider = ({ fireNumber }) => {
@@ -20,7 +20,7 @@ const MeterSlider = ({ fireNumber }) => {
 
     return (
         <div className="w-100 bg-light-gray">
-            <div className="w-70 w-60-m w-50-l ph3 mw8 mv2 mv4-ns center flex items-center">
+            <div className="w-70 w-60-m w-50-l ph3 mw8 mt2 mt4-ns center flex items-center">
                 <span className="pr2 f3" style={{ color: iceColor }}>
                     <FontAwesomeIcon icon={faSnowflake} />
                 </span>
@@ -42,7 +42,20 @@ const MeterSlider = ({ fireNumber }) => {
                     <FontAwesomeIcon icon={faFire} />
                 </span>
             </div>
-
+            <div className="w-70 w-60-m w-50-l ph3 mw8 mb2 mb4-ns center flex items-center justify-between">
+                <span className="pr2 f6 f5-l" style={{ color: iceColor }}>
+                    專業
+                </span>
+                <span className="pr2 f5 f4-l" style={{ color: iceColor }}>
+                    <FontAwesomeIcon icon={faLongArrowAltLeft} />
+                </span>
+                <span className="pl2 f5 f4-l" style={{ color: fireColor }}>
+                    <FontAwesomeIcon icon={faLongArrowAltRight} />
+                </span>
+                <span className="pl2 f6 f5-l" style={{ color: fireColor }}>
+                    生活
+                </span>
+            </div>
         </div>
     )
 }
