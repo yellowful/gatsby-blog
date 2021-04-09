a0edf9efa0609aeda7327cd6a342d63dfbb4ce13
yellowfully <richenyou@gmail.com>
2021-04-09 17:09:26 +0800

    add link to head and image of the project cards

--- src/components/ProjectPage/ProjectCard.js
+++ src/components/ProjectPage/ProjectCard.js
@@ -9,7 +9,9 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 const ProjectCard = ({ slug, projectName, demoLink, repoLink, introduction, image }) => {
     return (
         <article className="pa2 br3 ma2 mv4-ns bg-moon-gray mh0-ns">
-            <h2 className="head-1-shadow f2 ph2 lh-title fw7 mv4 dark-gray">{projectName}</h2>
+            <Link to={`/project/${slug}/`}>
+                <h2 className="head-1-shadow f2 ph2 lh-title fw7 mv4 dark-gray">{projectName}</h2>
+            </Link>
             <div className="flex flex-column flex-row-ns">
                 <div key="project-card-section" className="w-100 w-50-m w-70-l pr3-ns order-2 order-1-ns ph2">
                     <section dangerouslySetInnerHTML={{ __html: introduction }} />
@@ -30,7 +32,9 @@ const ProjectCard = ({ slug, projectName, demoLink, repoLink, introduction, imag
                     </table>
                 </div>
                 <div key="project-card-image" className="pl3-ns order-1 order-2-ns mb4 mb0-ns ph2 center pt5-m w-50-m mw5-m w-30-l">
-                    <img className="br3" src={image} alt="demo of project" />
+                    <Link to={`/project/${slug}/`}>
+                        <img className="br3" src={image} alt="demo of project" />
+                    </Link>
                 </div>
             </div>
         </article>
