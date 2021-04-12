0cce0a7bbd43f81121d013913ea9937c4dadb3da
yellowfully <richenyou@gmail.com>
2021-04-12 16:01:29 +0800

    in the project buttons component, change link text to link div

--- src/components/ProjectButtons/ProjectButtons.js
+++ src/components/ProjectButtons/ProjectButtons.js
@@ -8,32 +8,32 @@ const ProjectBottons = ({ slug, demoLink, repoLink }) => {
 
     return (
         <footer className="w-100 flex justify-around-l flex-row-l items-center-l flex-column mv2">
-            <div key={`button-${demoLink}`} className="tc bg-mid-gray mv1 br2">
-                <a className="f4 fw3 pa2 near-white" href={demoLink} rel="noreferrer" target="_blank">
+            <a key={`button-${demoLink}`} href={demoLink} rel="noreferrer" target="_blank">
+                <div className="tc bg-mid-gray mv1 br2 f4 fw3 pa1 near-white">
                     試玩 &nbsp;<FontAwesomeIcon icon={faGamepad} />
-                </a>
-            </div>
-            <div key={`button-${repoLink}`} className="tc bg-mid-gray mv1 br2">
-                <a className="f4 fw3 pa2 near-white" href={repoLink} rel="noreferrer" target="_blank">
+                </div>
+            </a>
+            <a  key={`button-${repoLink}`} href={repoLink} rel="noreferrer" target="_blank">
+                <div className="tc bg-mid-gray mv1 br2 f4 fw3 pa1 near-white">
                     原始碼 &nbsp;<FontAwesomeIcon icon={faGithub} />
-                </a>
-            </div>
+                </div>
+            </a>
             {
                 slug ?
                     (
-                        <div key={`button-${slug}`} className="tc bg-mid-gray mv1 br2">
-                            <Link className="f4 fw3 pa2 near-white" to={`/project/${slug}/`}>
+                        <Link to={`/project/${slug}/`}>
+                            <div className="tc bg-mid-gray mv1 br2 f4 fw3 pa1 near-white">
                                 了解更多 &nbsp;<FontAwesomeIcon icon={faAngleDoubleRight} />
-                            </Link>
-                        </div>
+                            </div>
+                        </Link>
                     )
                     :
                     (
-                        <div key="back-to-project" className="tc bg-mid-gray mv1 br2">
-                            <Link className="f4 fw3 pa2 near-white" to="/project/">
+                        <Link to="/project/">
+                            <div className="tc bg-mid-gray mv1 br2 f4 fw3 pa1 near-white">
                                 回作品集 &nbsp;<FontAwesomeIcon icon={faAngleDoubleLeft} />
-                            </Link>
-                        </div>
+                            </div>
+                        </Link>
                     )
             }
         </footer>
