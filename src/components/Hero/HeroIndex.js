961a3d166834576a3eb34aa320e16b97108635c3
yellowfully <richenyou@gmail.com>
2021-05-18 16:24:46 +0800

    adjust layout of hero of index

--- src/components/Hero/HeroIndex.js
+++ src/components/Hero/HeroIndex.js
@@ -5,37 +5,39 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
 
 //用來顯示index page的最上面hero的畫面
-const HeroIndex = ({bgImage,slogan}) => {
+const HeroIndex = ({ bgImage, slogan }) => {
     return (
         <section className="w-100">
-                <BackgroundImage
-                    Tag={"div"}
-                    className="hero is-fullheight-with-navbar"
-                    {...bgImage}
-                    backgroundColor={`#000`}
-                >
-                    <div className="w-100 h-100 absolute clip-path-hero flex flex-column justify-center">
-                        <div className="columns ma2 ma3-ns">
-                            <div className="column">
-                                <h1 key="hero-slogan-1" className="f2-l f3-m f4 light-silver tc">
-                                    {slogan[0]}
-                                </h1>
-                                <h1 key="hero-slogan-2" className="f2-l f3-m f4 light-silver tc">
-                                    {slogan[1]}
-                                </h1>
-                            </div>
-                            <footer className="column flex flex-column justify-end">
+            <BackgroundImage
+                Tag={"div"}
+                className="hero is-fullheight-with-navbar"
+                {...bgImage}
+                backgroundColor={`#000`}
+            >
+                <div className="w-100 h-100 absolute clip-path-hero flex flex-column justify-center">
+                    <div className="columns w-100">
+                        <div className="column">
+                            <h1 key="hero-slogan-1" className="f2-l f3-m f4 light-silver tc">
+                                {slogan[0]}
+                            </h1>
+                            <h1 key="hero-slogan-2" className="f2-l f3-m f4 light-silver tc">
+                                {slogan[1]}
+                            </h1>
+                        </div>
+                        <footer className="column tr tc-l">
+                            <div className="mt2 mt5-ns">
                                 <Link
                                     to="/about/#bio"
                                     replace={false}
-                                    className="ma2 f3-l f4-m f5  tr tr-m tc-l"
+                                    className="f3-l f4-m f5"
                                 >
                                     關於我<FontAwesomeIcon icon={faAngleDoubleRight} />
                                 </Link>
-                            </footer>
-                        </div>
+                            </div>
+                        </footer>
                     </div>
-                </BackgroundImage>
+                </div>
+            </BackgroundImage>
         </section>
     )
 }
